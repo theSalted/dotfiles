40fa5e6ce0f0fb123399997cba2fdfdc1367472a
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-06-10 03:48:44 +0800

    reinit fish and starship

---
+++ config/fish/config.fish
@@ -0,0 +1,13 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+
+# Created by `pipx` on 2024-05-16 12:44:29
+set PATH $PATH /Users/yuhaochen/.local/bin
+
+starship init fish | source
