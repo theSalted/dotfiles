fbf40c95b513432ef124323cc20f1115b29cd0e4
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-03-17 16:00:57 -0500

    update fish zellij config

--- .config/fish/auto_start_zellij.fish
+++ .config/fish/auto_start_zellij.fish
@@ -1,4 +1,12 @@
-if set -q ZELLIJ
-else
-  zellij attach
+if type -q ZELLIJ
+    # check if we're not already inside a zellij session
+    if not set -q ZELLIJ
+        # try to attach to an existing session 
+        zellij attach
+
+        # if attaching failed (no such session), create a new session
+        if test $status -ne 0
+            zellij
+        end
+    end
 end
